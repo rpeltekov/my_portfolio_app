7d122fcdfdba72e3835267cb1a3aa2eea89265d1
Robert Peltekov <48700626+anonymoussecurity0@users.noreply.github.com>
2019-12-29 03:47:13 -0800

    pretty much all of the info is there now. added activities section. working on contact form

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,21 +6,30 @@ class Resume extends Component {
         if (this.props.data) {
             var skillmessage = this.props.data.skillmessage;
             var education = this.props.data.education.map(function (education) {
-                return <div key={education.school}><h3>{education.school}</h3>
-                    <p className="info">{education.degree} <span><br/></span><em
-                        className="date">{education.graduated}</em></p>
-                    <p>{education.description}</p></div>
+                return <div key={education.school}><h3>{education.school} <span>&#8226;</span> <em className="date">{education.graduated}</em></h3>
+                    <p className="info">{education.degree}</p>
+                    <p>{education.description}</p>
+                </div>
             })
             var work = this.props.data.work.map(function (work) {
-                return <div key={work.company}><h3>{work.company}</h3>
-                    <p className="info">{work.title}<span><br/></span> <em className="date">{work.years}</em></p>
+                return <div key={work.company}><h3>{work.company} <span>&#8226;</span> <em className="date">{work.years}</em></h3>
+                    <p className="info">{work.title}</p>
                     <p>{work.description}</p>
+                </div>
+            })
+            var activities = this.props.data.activities.map(function (activities) {
+                return <div key={activities.name}>
+                        <h4>{activities.name} <span>&#8226;</span> <em className="date">{activities.years}</em></h4>
+                    <h5>{activities.description1}</h5>
+                    <p className="info">
+                        {activities.position}
+                    </p>
+                    <p>{activities.description2}</p>
                 </div>
             })
             var skills = this.props.data.skills.map(function (skills) {
                 var className = 'bar-expand ' + skills.name.toLowerCase();
-                return <li key={skills.name}><span style={{width: skills.level}}
-                                                   className={className}></span><em>{skills.name}</em></li>
+                return <li key={skills.name}><em>{skills.name}</em></li>
             })
         }
 
@@ -40,31 +49,28 @@ class Resume extends Component {
                         </div>
                     </div>
                 </div>
-
-
                 <div className="row work">
-
                     <div className="three columns header-col">
                         <h1><span>Work</span></h1>
                     </div>
-
                     <div className="nine columns main-col">
                         {work}
                     </div>
                 </div>
-
-
+                <div className="row activities">
+                    <div className="three columns header-col">
+                        <h1><span>Activities</span></h1>
+                    </div>
+                    <div className="nine columns main-col">
+                        {activities}
+                    </div>
+                </div>
                 <div className="row skill">
-
                     <div className="three columns header-col">
                         <h1><span>Skills</span></h1>
                     </div>
-
                     <div className="nine columns main-col">
-
-                        <p>{skillmessage}
-                        </p>
-
+                        <p>{skillmessage}</p>
                         <div className="bars">
                             <ul className="skills">
                                 {skills}
